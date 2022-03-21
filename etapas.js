bc74fbbbffab292810376ab19849d66dfeb1a1d3
mathspin <matheuszsp@gmail.com>
2022-03-21 17:34:41 -0300

    Primeiro commit
    Primeiro projeto do video no youtube que possui 3 projetos

---
+++ etapas.js
@@ -0,0 +1,48 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros:5,
+        candidatos: [
+            {
+            numero: '38111',
+            nome: 'João',
+            partido: 'ABC',
+            fotos: [
+                {url: 'img1.png', legenda: 'Vereador'}
+            ]
+            },
+            {
+            numero: '77222',
+            nome: 'Maria',
+            partido: 'DEF',
+            fotos: [
+                {url: 'img2.png', legenda: 'Vereador'}
+            ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros:2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Joana',
+                partido: 'GHI',
+                fotos: [
+                    {url: 'img3.png', legenda: 'Prefeito'},
+                    {url: 'img4.png', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Marcos',
+                partido: 'JKL',
+                fotos: [
+                    {url: 'img5.png', legenda: 'Prefeito'},
+                    {url: 'img6.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }     
+];
